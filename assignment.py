a7514aa21f27650e03a11ebccb62a9dfbf056268
psolanky08 <75457120+psolanky08@users.noreply.github.com>
2022-04-17 21:46:51 -0400

    preprocessing with slicing done

--- assignment.py
+++ assignment.py
@@ -2,8 +2,8 @@ from preprocess import *
 
 def main(): 
     i_val = 11 #change this from 1-51 
-    image_slices = get_data(i_val)
-    print("num of slices: ", len(image_slices))
+    data, labels = get_data(i_val)
+    print("num of slices: ", len(data))
 
 if __name__ == '__main__':
 	main()
